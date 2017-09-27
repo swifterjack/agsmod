a4e7150149d80d27faf7c153b3c3df3249afda36
swifterjack <swifterjack1@gmail.com>
2017-09-26 17:27:51 -0700

    added buffs

---
+++ AGSmod/Buffs/fodlepet.cs
@@ -0,0 +1,27 @@
+using Terraria;
+using Terraria.ModLoader;
+
+namespace AGSmod.Buffs
+{
+	public class fodlepet : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Fodle Pet");
+			Description.SetDefault("\"Play Ghoulies Dungeon!\"");
+			Main.buffNoTimeDisplay[Type] = true;
+			Main.vanityPet[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.buffTime[buffIndex] = 18000;
+            player.GetModPlayer<AGSplayer>(mod).fodlepet = true;
+			bool petProjectileNotSpawned = player.ownedProjectileCounts[mod.ProjectileType("fodlepet")] <= 0;
+			if (petProjectileNotSpawned && player.whoAmI == Main.myPlayer)
+			{
+				Projectile.NewProjectile(player.position.X + (float)(player.width / 2), player.position.Y + (float)(player.height / 2), 0f, 0f, mod.ProjectileType("fodlepet"), 0, 0f, player.whoAmI, 0f, 0f);
+			}
+		}
+	}
+}
