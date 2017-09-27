95f265c3df890c8163245c947991d15398022aea
swifterjack <swifterjack1@gmail.com>
2017-09-26 17:31:48 -0700

    Add files via upload

--- AGSmod/Items/Fodle.cs
+++ AGSmod/Items/Fodle.cs
@@ -1,38 +1,40 @@
 using Terraria;
-using Terraria.ModLoader;
 using Terraria.ID;
+using Terraria.ModLoader;
 
 namespace AGSmod.Items
 {
-        public class Fodle : ModItem
-        {
-            public override void SetStaticDefaults()
-            {   
-                DisplayName.SetDefault("Fodle");
-                Tooltip.SetDefault("Happy birthday!");
-            }
-            
-            public override void SetDefaults()
-            {
-                item.useStyle = 4;
-                item.useTime = 16;
-                item.expert = true;
-                item.rare = 3;
-                item.stack = 1;
-                item.useAnimation = 16;
-                item.autoReuse = true;
-                item.makeNPC = 	398;
-            }
-            
-            public override void AddRecipes()
+	public class Fodle : ModItem
+	{
+		public override void SetStaticDefaults()
 		{
-			ModRecipe recipe = new ModRecipe(mod);
-			recipe.AddIngredient(ItemID.DesertGhoulBanner, 1);
-            recipe.AddIngredient(ItemID.MusicBoxDungeon, 1);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.SetResult(this);
-			recipe.AddRecipe();
+			DisplayName.SetDefault("Fodle");
+			Tooltip.SetDefault("Happy Birthday!");
 		}
 
+		public override void SetDefaults()
+		{
+			item.CloneDefaults(ItemID.CursedSapling);
+			item.shoot = mod.ProjectileType("fodlepet");
+			item.buffType = mod.BuffType("fodlepet");
+		}
+
+		public override void AddRecipes()
+		{
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DesertGhoulBanner, 1);
+            recipe.AddIngredient(ItemID.MusicBoxDungeon, 1);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
+
+		public override void UseStyle(Player player)
+		{
+			if (player.whoAmI == Main.myPlayer && player.itemTime == 0)
+			{
+				player.AddBuff(item.buffType, 3600, true);
+			}
+		}
+	}
 }
